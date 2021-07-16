47c60eed58450b6ab4a2830f234a5e566d4fe6a9
Naufal Hakim Syahputra <naufalhsyahputra@gmail.com>
2021-07-16 21:33:12 +0700

    refactor(server): now use env instead hard-coded build(docker): Add Mysql chore(gitignore): Add env

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen(8080, '0.0.0.0');
+    await fastify.listen(process.env.PORT, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
